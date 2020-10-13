3ae7b0f58e9b7a727d72142ffa151a591fd8fa11
D'artagnan <roymartinezramirez@gmail.com>
2020-10-13 16:11:46 -0500

    Add project files.

---
+++ XAMLHelperCore/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XAMLHelperCore
+{
+    public class Class1
+    {
+    }
+}
